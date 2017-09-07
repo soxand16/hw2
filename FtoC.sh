a16171106fa6b69ba29456987ae3600533a21182
Sam <soxand16@aol.com>
2017-09-06 21:30:23 -0500

    FtoC also give temp in Kelvin

--- FtoC.sh
+++ FtoC.sh
@@ -1,2 +1,4 @@
 #!bin/bash/
-bc <<<  "scale=2; ($1-32)*5/9"
+c=$(echo "scale=2; ($1-32)*5/9" | bc)
+K=$(echo "scale=2; ($1-32)*5/9 + 273" | bc)	
+echo "The temperature is $c degrees C or $K Kelvin."
